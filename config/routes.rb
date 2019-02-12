9a845f2111c2d0ae8bb36c2418986a58d7481967
miwashun <miwashun@miwanomadokanoMacBook-Pro.local>
2019-02-12 18:47:09 +0900

    rewrite_routes.rb
    メッセージテーブルが無くても先にリソースを作っておく

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  root  'messages#index'
   root 'groups#index'
   devise_for :users
-  get 'messages' => 'messages#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
-
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
