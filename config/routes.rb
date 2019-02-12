8c117110ed8df01f59aa0943b6d285987455fae4
miwashun <miwashun@miwanomadokanoMacBook-Pro.local>
2019-02-12 10:40:08 +0900

    creating_db
    グループと中間テーブルのモデル、コントローラ、アソシエーションの作成
    ２回ぐらい消したからあとで不具合が出るかも

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages' => 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 
 end
