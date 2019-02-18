27e899558d92aad1a65c02f5c2a79c65a3c4f85f
miwashun <miwashun@miwanomadokanoMacBook-Pro.local>
2019-02-18 17:40:57 +0900

    modify
    Newアクションではなくcreateアクションに変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -18,7 +18,7 @@ $(function(){
                 </div>`
                 return html;
   }
-  $('#new_message').on('submit', function(e){
+  $('#create_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     $.ajax({
