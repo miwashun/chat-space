3d261d8eef212a7eddd48093c0f7535f4982c889
miwashun <miwashun@miwanomadokanoMacBook-Pro.local>
2019-03-05 13:12:31 +0900

    Mystery

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,7 +16,6 @@ class MessagesController < ApplicationController
       respond_to do |format|
         format.json
       end
-
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
